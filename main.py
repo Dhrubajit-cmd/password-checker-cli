a3de677fc0731c2b80a529f693898f3cc38b889d
Dhrubajit-cmd <dhrubajitchakravorty@gmail.com>
2026-06-25 01:14:41 +0530

    feat: Added check_password_strength and check_another_password function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-if __name__ == __main__ : 
-    main() 
+if __name__ ==  '__main__' : 
+   print("===== 🔑 Welcome to Password Strength Checker 🔑 =====")
+   while check_another_password():
+        check_password_strength()
 
 
